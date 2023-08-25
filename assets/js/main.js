f17ef4a1f42e13ba440de66aa21a0a5fbafac687
SemTheLegend <semmathe@gmail.com>
2023-08-25 15:33:35 +0200

    added reveal function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,84 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
-/* ======== SHOW MENU ======= */
-// Validate if content exists
+/*===== SHOW MENU =====*/
+// Validate if constant exists
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
-/* ========= HIDE MENU ========= */
-// Validate if content exists
+/*===== HIDE MENU =====*/
+/* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
   });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelector(".nav__link");
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-  const navMenu = document.getElementById("nev-menu");
+  const navMenu = document.getElementById('nav-menu')
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
+  navMenu.classList.remove('show-menu')
 }
-
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-  const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, ad the scroll-header class to the header tag
-  // this.scrollY >= 50 ? header.classList.add("scroll-header") : header.classList.remove("scroll-header");
-  if (this.scrollY >= 50) {
-    header.classList.add("scroll-header");
-  } else {
-    header.classList.remove("scroll-header");
-  }
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/
+/*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
+  const scrollUp = document.getElementById('scroll-up');
   // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scroll >= 200) {
-    scrollUp.classList.add("show-scroll");
-  } else {
-    scrollUp.classList.remove("show-scroll");
-  }
+  if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
-window.addEventListener("scroll", scrollUp);
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset
 
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    sectionId = current.getAttribute('id')
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
     } else {
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
     }
   });
 }
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+})
 
+sr.reveal(`.home__header, .section__title`, { delay: 600 })
+sr.reveal(`.home__footer`, { delay: 700 })
+sr.reveal(`.home__img`, { delay: 900, origin: 'top' })
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, { origin: 'top', interval: 100 })
+sr.reveal(`.specs__data, .discount__animate`, { origin: 'left', interval: 100 })
+sr.reveal(`.specs__img, .discount__img`, { origin: 'right' })
+sr.reveal(`.case__img`, { origin: 'top' })
+sr.reveal(`.case__data`)
