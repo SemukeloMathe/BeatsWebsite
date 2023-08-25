a70c028be2ff6ac0a0ae218aeb59db5eb1b47271
SemTheLegend <semmathe@gmail.com>
2023-08-25 11:54:37 +0200

    show menu functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* ======== SHOW MENU ======= */
+// Validate if content exists
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* ========= HIDE MENU ========= */
+// Validate if content exists
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
@@ -7,7 +25,7 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
